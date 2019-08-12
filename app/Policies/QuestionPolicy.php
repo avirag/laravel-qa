f89afd3f45b42086909a8d13ab865ccc527e9813
Anita Virag <ik.virag@gmail.com>
2019-08-12 20:45:21 +0200

    designing answer schema

--- app/Policies/QuestionPolicy.php
+++ app/Policies/QuestionPolicy.php
@@ -9,7 +9,7 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 class QuestionPolicy
 {
     use HandlesAuthorization;
-    
+
     /**
      * Determine whether the user can update the question.
      *
@@ -31,7 +31,7 @@ class QuestionPolicy
      */
     public function delete(User $user, Question $question)
     {
-        return $user->id === $question->user_id && $question->answers < 1;
+        return $user->id === $question->user_id && $question->answers_count < 1;
     }
 
     /**
